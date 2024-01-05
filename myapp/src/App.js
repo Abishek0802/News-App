42355cb2c62fa77b293a1d12f70dea2ec628709d
Abishek0802 <abishek20td0802@gmail.com>
2024-01-05 21:56:56 +0530

    ..

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Home/Header";
 import Product_List from "./Home/Product_List";
 import Result from "./Transfer-data-to-one-to-another/Result";
+import Maping from "./API-loop/Maping";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Product_List />
       <Result />
+      <Maping />
     </>
   );
 }
